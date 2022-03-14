395bd332c11e4740972d4f8030cf366f5b57894d
Martin Hauge <maha@kb.dk>
2022-03-14 16:14:31 +0100

    requester refactoring

--- requester.py
+++ requester.py
@@ -1,90 +1,102 @@
-# UN Comtrade API
+# UN Comtrade API Bulk data extraction 
 
 import requests
 import zipfile
 import io
 
-import pandas as pd
 from pathlib import Path
+import pandas as pd
 import json
 
-def requester(config):
-	base_url = config['url']
+class DataRequester:
+	def __init__(self, config):
+		if not check_parameters(config):
+			raise Exception('Configuration parameters could not be loaded. Check config file.')
+		
+		self.params = config['params']
+		self.url = config['url']
+		self.request_url = self.url + '/api//refs/da/bulk?'
+		self.start_year = config['start_year']
+		self.end_year = config['end_year']
 
-	req_url = base_url + '/api//refs/da/bulk?'
+		self.date_range = generate_date_range(self.start_year, self.end_year, self.params['freq'])
 
-	start_year = config['start_year']
-	end_year = config['end_year']
+		self.save_folder = Path(config['data_folder'])
 
-	date_list = generate_date_range(start_year, end_year)
+		if not self.save_folder.is_dir():
+			self.save_folder.mkdir(parents=True)
+		
 
-	params = config['params']
 
-	save_folder = Path('data', 'raw')
+	def call_api(self):
+		res = requests.get(self.request_url, params=self.params)
 
-	if not save_folder.is_dir():
-		save_folder.mkdir(parents=True)
+		if not res.url.startswith(self.url):
+			raise Exception('Authentication error: Make sure access through IP address is granted and check URL in config file. Returned URL:', res.url)
+		if res.status_code == 200:
+			self.response_data = res.json()
+		else:
+			raise Exception('Unsuccessful request:', res.status_code)
 
-	res = requests.get(req_url, params=params)
-
-	if not res.url.startswith(config['url']):
-		raise Exception('Authentication error:', res.url)
-	if res.status_code == 200:
-		print(res.url)
-		data = res.json()
-
-		for i in data:
-			if i['ps'] in date_list:
-				if not Path(save_folder, i['name'].replace('zip', 'csv')) in save_folder.iterdir():
-					save_csv(i, save_folder, base_url)
+	def request_data(self):
+		for item in self.response_data:
+			if item['ps'] in self.date_range:
+				file_path = Path(self.save_folder, item['name'].replace('zip', 'csv'))
+				if not file_path.exists(): # in self.save_folder.iterdir():
+					self.save_csv(item, file_path)
 				else:
-					print(i['name'], 'already exists. Skipping...')
-	else:
-		raise Exception(f'Bad response: {res.status_code}')
+					print(item['name'], 'already extracted. Skipping...')
 
-def save_csv(data, save_folder, url):
-	res = requests.get(url + data['downloadUri'])
-	if res.status_code == 200:
-		
-		print('Saving', data['name'])
-		z = zipfile.ZipFile(io.BytesIO(res.content))
-		z.extractall(save_folder)
-		
-	else:
-		raise Exception(f'Bad response: {res.status_code} ({res.url})\n{data}')
+	def save_csv(self, item, file_path):
+		res = requests.get(self.url + item['downloadUri'])
+		if res.status_code == 200:
+			print('Saving', item['name'])
+			z = zipfile.ZipFile(io.BytesIO(res.content))
+			z.extractall(self.save_folder)
+		else:
+			raise Exception(f'Unsuccessful request: {res.status_code} ({res.url})\n{item}')
 
-def combiner(config):
-	data_folder =Path('data', 'raw')
+class DataMerger:
+	def __init__(self, config):
+		self.data_folder = Path(config['data_folder'])
+		self.output_file = config['output_file_name']
 
-	output_file = config['output_file_name']
+		self.trade_flow_filter = config['trade_flow']
+		self.classification_filter = config['detailed_classification']
 
-	trade_flow_filter  = config['trade_flow']
-	classificaton_filter = config['classification_detail']
+		self.df = pd.DataFrame()
 
-	df = pd.DataFrame()
+	def merge_files(self):
+		for f in self.data_folder.iterdir():
+			if f.name.endswith('csv'):
+				temp_df = pd.read_csv(f)
+				if self.trade_flow_filter in ['Imports', 'Exports', 'Re-imports', 'Re-exports']:
+					temp_df = temp_df.loc[temp_df['Trade Flow'] == self.trade_flow_filter]
+				if self.classification_filter in [2, 4, 6]:
+					temp_df = temp_df.loc[temp_df['Commodity Code'].str.len() == self.classification_filter]
+				self.df = self.df.append(temp_df)
 
-	for f in data_folder.iterdir():
-		if f.name.endswith('csv'):
-			print(f)
-			temp_df = pd.read_csv(f)
-			if trade_flow_filter:
-				if trade_flow_filter in ['Imports', 'Exports', 'Re-imports', 'Re-exports']:
-					temp_df = temp_df.loc[temp_df['Trade Flow'] == trade_flow_filter]
-			if classificaton_filter:
-				if classificaton_filter in [2, 4, 6]:
-					temp_df = temp_df.loc[temp_df['Commodity Code'].str.len() == classificaton_filter]
+	def save_data(self):
+		self.df = self.df.sort_values(by='Period')
+		self.df.to_csv(self.output_file, index=False)
 
-			df = df.append(temp_df)
 
-	df = df.sort_values(by='Period')
-	df.to_csv(output_file, index=False)
-	print('Data saved to:', output_file)
+def check_parameters(config):
+	return isinstance(config['url'], str) and \
+	isinstance(config['start_year'], int) and \
+	isinstance(config['end_year'], int) and \
+	isinstance(config['output_file_name'], str) and \
+	isinstance(config['trade_flow'], str) and \
+	isinstance(config['detailed_classification'], int)	
 
-def generate_date_range(start_year, end_year):
+def generate_date_range(start_year, end_year, freq):
 	date_list = []
 	for y in range(start_year, end_year + 1):
-		for m in range(12):
-			date_list.append(f'{y}{m + 1:02}')
+		if freq == 'M':
+			for m in range(12):
+				date_list.append(f'{y}{m + 1:02}')
+		else:
+			date_list.append(y)
 
 	return date_list
 
@@ -94,14 +106,22 @@ def parse_config(config_path):
 
 	return json.loads(config_file)
 
-if __name__ == '__main__':
-
-	config_file = 'config.json'
+def main(config_file):
 
 	config = parse_config(config_file)
 
-	requester(config)
-	combiner(config)
+	#data_folder = Path('data', 'raw') # TODO: Add to config
 
-
+	requester = DataRequester(config)
+	requester.call_api()
+	requester.request_data()
 	
+	merger = DataMerger(config)
+	merger.merge_files()
+	merger.save_data()
+	
+
+if __name__ == '__main__':
+	config_file = 'config.json'
+
+	main(config_file)
